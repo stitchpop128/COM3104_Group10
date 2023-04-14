2239fb724701dfcf0feebca2ac38e0da67a1daa0
Cyrus Lau <cyruslau.work@gmail.com>
2023-04-14 16:15:43 +0800

    add Bottom Navbar v2

--- app/src/main/java/com/example/com3104_group10/MainActivity.java
+++ app/src/main/java/com/example/com3104_group10/MainActivity.java
@@ -3,18 +3,23 @@ package com.example.com3104_group10;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.com3104_group10.fragments.CalculatorFragment;
+import com.example.com3104_group10.fragments.DrawingFragment;
+import com.example.com3104_group10.fragments.MapFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Drawing extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawing);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.layout,new DrawingFragment()).commit();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setSelectedItemId(R.id.dra);
@@ -23,14 +28,16 @@ public class Drawing extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.dra:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.layout,new DrawingFragment()).commit();
                         return true;
                     case R.id.cal:
-                        startActivity(new Intent(getApplicationContext(), Calculator.class));
-                        overridePendingTransition(0, 0);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.layout,new CalculatorFragment()).commit();
                         return true;
                     case R.id.map:
-                        startActivity(new Intent(getApplicationContext(), Map.class));
-                        overridePendingTransition(0, 0);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.layout,new MapFragment()).commit();
                         return true;
                 }
                 return false;
