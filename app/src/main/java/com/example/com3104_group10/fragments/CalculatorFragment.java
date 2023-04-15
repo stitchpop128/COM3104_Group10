d38a14818057f4d104181a3b20cad2092fd87a7a
lilyx <lilypang1999@gmail.com>
2023-04-15 15:54:16 +0800

    Calculator UI basic done

--- app/src/main/java/com/example/com3104_group10/fragments/CalculatorFragment.java
+++ app/src/main/java/com/example/com3104_group10/fragments/CalculatorFragment.java
@@ -51,6 +51,8 @@ public class CalculatorFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
